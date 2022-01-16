71b58243c2fb19452a61acbaf6b6150c3b5bdca3
Duc Tri Le <dle@tripadvisor.com>
2022-01-16 12:55:33 -0500

    Another try at correctly distributing binaries

---
+++ packages/aide-build-tools/create-empty-binaries.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+BASE_DIR="$(dirname "$(realpath "$0")")"
+
+# When developing locally, the dist folder would not yet exists and hence the binary would be missing causing
+# installation to fail. To get around it, we customized rush.json to call this script to create empty files to allow
+# installation, then build it after the install
+mkdir -p $BASE_DIR/dist/bin
+touch $BASE_DIR/dist/bin/run-eslint.js
+touch $BASE_DIR/dist/bin/run-linter.js
+touch $BASE_DIR/dist/bin/run-prettier.js
